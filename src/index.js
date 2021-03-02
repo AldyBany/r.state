e00585246afe5abf8dacf271c202fecb9bf025bf
Aldy Bany <64736339+AldyBany@users.noreply.github.com>
2021-03-02 10:39:44 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper("#property-slider .swiper-container", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true
+});
+
+const swipe = new Swiper("#testimonial-slider.swiper-container", {
+  // Optional parameters
+  loop: true
+});
